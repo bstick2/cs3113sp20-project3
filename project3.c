51865efac77d7f932341e193f81783e1ea6fdeb5
bstick2 <47426528+bstick2@users.noreply.github.com>
2020-05-03 14:11:30 -0500

    updated some comments

--- project3.c
+++ project3.c
@@ -5,18 +5,19 @@
 #define FALSE = 0;
 
 typdef struct process {
-	int pid;				//process id
-	int priority;			
+	int pid;			//process id
+	int priority;			//priority level of the process
 	int arrTime;			//arrival time
 	int compTime;			//completion time
 	int cpuBurst;			//burst time
 	int waitTime;			//waiting time
 	int respTime;			//response time
-	int taTime;				//turnaround time
+	int taTime;			//turnaround time
 	int cpuBurstLeft;
 }process;
 
-int firstFit(int blockSize[], int m, int processSize[], int n){
+int firstFit(int blockSize[], int m, int processSize[], int n)
+{
 	int allocation[n];
 
 	memset(allocation, -1, sizeof(allocation));
@@ -38,23 +39,28 @@ int firstFit(int blockSize[], int m, int processSize[], int n){
 
 	for (int i = 0; i < n; i++)
 	{
-		if (allocation[i] != -1) {
+		if (allocation[i] != -1) 
+		{
 			allocation[i]++;
 			return 1;
+		}
 		else return 0;			//returns 0 because the process was not allocated.
 	}
 }
 
-void buddy(){
+void buddy()
+{
 
 }
 
-void roundRobin(int timeQuantum){
+void roundRobin(int timeQuantum)
+{
 	
 }
 
 
-int main(){
+int main()
+{
 	process myProcesses[MAX];
 	int q; 						//quantum
 	int p;						//number of concurrent pocesses
